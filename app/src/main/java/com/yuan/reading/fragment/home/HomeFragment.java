efe27cf2f197be642cc61479ecc2209f1f2b16f0
YCW1997 <15636333601@163.com>
2019-03-01 22:10:34 +0800

    childrenfragment

--- app/src/main/java/com/yuan/reading/fragment/home/HomeFragment.java
+++ app/src/main/java/com/yuan/reading/fragment/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ActionMenuView;
 import android.support.v7.widget.Toolbar;
@@ -11,11 +13,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.yuan.reading.R;
+import com.yuan.reading.fragment.home.childrenfragment.AndroidFragment;
+import com.yuan.reading.fragment.home.childrenfragment.CateFragment;
+import com.yuan.reading.fragment.home.childrenfragment.HotFragment;
 
 /**
  * Created by Administrator on 2019/2/21 0021.
@@ -28,8 +34,38 @@ public class HomeFragment extends Fragment{
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
         inflater.inflate(R.menu.img,menu);
+    }
+
+    FragmentManager fm=getChildFragmentManager();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.android:
+                Fragment androidFragment = new AndroidFragment();
+                FragmentTransaction transaction=fm.beginTransaction();
+                if(!androidFragment.isAdded()){
+                    transaction.add(R.id.android_fragment, androidFragment).commit();
+                }
+                return true;
+            case R.id.hot:
+                Fragment hotFragment = new HotFragment();
+                FragmentTransaction transaction2 = fm.beginTransaction();
+                if(!hotFragment.isAdded()) {
+                    transaction2.add(R.id.hot_fragment, hotFragment).commit();
+                }
+                return true;
+            case R.id.cate:
+                Fragment cateFragment = new CateFragment();
+                FragmentTransaction transaction3 = fm.beginTransaction();
+                if(!cateFragment.isAdded()) {
+                    transaction3.add(R.id.cate_fragment, cateFragment).commit();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Nullable
---
+++ app/src/main/java/com/yuan/reading/fragment/home/childrenfragment/HotFragment.java
@@ -0,0 +1,26 @@
+package com.yuan.reading.fragment.home.childrenfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yuan.reading.R;
+
+/**
+ * Created by Administrator on 2019/3/1 0001.
+ */
+
+public class HotFragment extends Fragment {
+    View mView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (mView == null) {
+            mView = inflater.inflate(R.layout.hot_fragment, null);
+        }
+        return mView;
+    }
+}
